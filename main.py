042a92539a15f38cdf205d1c5e679ea1e669db19
develop-insight <82137104+develop-insight@users.noreply.github.com>
2025-06-03 11:28:39 +0900

    [Feature] add main.py (#3)
    * add main.py
    
    * 의미없는 커밋
    
    ---------
    
    Co-authored-by: SuYeon Park <richred@richred.local>

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+이 코드는 'hello world'를 출력하는 간단한 프로그램입니다.
+"""
+
+
+if __name__ == "__main__":
+    # This is the main entry point of the program
+    # You can add your code here
+    print("hello world")
